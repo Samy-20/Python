3ea4278d40e906a151a31240b4b75d790bf73a2c
Sahil zagade <148172621+Samy-20@users.noreply.github.com>
2026-01-02 17:47:59 +0530

    python.py

--- Basic_python/python.py
+++ Basic_python/python.py
@@ -1,4 +1,15 @@
 # Python is object-oriented programming language
+# print("Hello Sahil")
 
-print("Hello Sahil")
+name = " sahili  -"
+# print(name.title())
+
+# nm = name.title()
+# print(name)
+
+# print(name.index('i'))
+print(name.count('i', 2, -2))
+
+print(name.split(sep='a', maxsplit=-1))
+
 
